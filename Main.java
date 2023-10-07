9100d6b5ece69d9a58f4401aa5ca32578ee77f07
snoopy100 <63325516+snoopy100@users.noreply.github.com>
2023-10-07 12:24:54 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    Algorithim genetic-alg = new Algorithim(100);
+    // add alg class with evolve, muate, and crossover
+    // add Population class (i will copy and paste)
+    // add Chromsomse class (copy and paste, but chane to work with game)
+  }
+}
